22f103d6743b44f81fc044d9e767aa6bb7e1e9d3
Stefanie Brauers <stefanie.brauers@email.de>
2014-08-13 14:37:22 +0200

    Pushing new changes

--- source/app/web/application/catalog_browsing/ViewTheMainDepartments.cs
+++ source/app/web/application/catalog_browsing/ViewTheMainDepartments.cs
@@ -4,9 +4,16 @@ namespace app.web.application.catalog_browsing
 {
   public class ViewTheMainDepartments : ISupportAUserStory
   {
-    public void process(IProvideRequestDetails request)
+      private readonly IFindDepartments _department_finder;
+
+      public ViewTheMainDepartments(IFindDepartments department_finder)
+      {
+          _department_finder = department_finder;
+      }
+
+      public void process(IProvideRequestDetails request)
     {
-      throw new System.NotImplementedException();
+      _department_finder.get_the_main_departments(); 
     }
   }
 }
